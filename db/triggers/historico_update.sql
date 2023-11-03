debfc95850718a317afac5aa6a98739726efe7f3
Miguel Soares Francisco <miguel.soares.francisco@ua.pt>
2023-11-03 19:51:11 +0000

    u

--- db/triggers/historico_update.sql
+++
@@ -1,9 +0,0 @@
-USE sio-store;
-GO
-
-CREATE TRIGGER compra_efetuada
-AFTER INSERT ON Compras FOR EACH ROW
-BEGIN
-    INSERT INTO historico_compras (cliente_id, produto_id, data_compra, quantidade, preco_total)
-    VALUES (NEW.cliente_id, NEW.produto_id, NEW.data_compra, NEW.quantidade, NEW.preco_total);
-END;
